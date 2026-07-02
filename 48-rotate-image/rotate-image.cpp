54681bd0d78339c9b9827648f24a0040f48c9bc8
Shivang Kumar <saliyanshivang0@gmail.com>
2026-07-03 00:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.39%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int start=0;
-        int end = matrix.size()-1;
-        while(start<end){
-            for(int i =0;i<matrix[0].size();i++){
-                int temp=matrix[start][i];
-                matrix[start][i]=matrix[end][i];
-                matrix[end][i]=temp;
-            }
-            start++;
-            end--;
-        }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=i+1;j<matrix[i].size();j++){
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
+        }
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
         
     }
