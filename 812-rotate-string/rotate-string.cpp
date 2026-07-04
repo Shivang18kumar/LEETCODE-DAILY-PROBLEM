21c1922cd7b4261e0d7eb1f22e656d22cc0873b7
Shivang Kumar <saliyanshivang0@gmail.com>
2026-07-04 21:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (95.46%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-//this is not the opm=timized approac(its optimized in space with o(1) check other submision for time optimized)
-    bool check (string &s, string &goal,int shift){
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[(i+shift)%n]!=goal[i]) return false;
-        }
-        return true;
-    }
-
     bool rotateString(string s, string goal) {
-        int n=s.size();
+        string temp = s+s;
+        if(s.length()!=goal.length()) return false;
 
-        if(s.size()!= goal.size()){
-            return false;
-        }
-        for(int shift=0;shift<n;shift++){
-            if(check(s,goal,shift)) return true;
-        }
-        return false;
+        if(temp.find(goal)!=string::npos) return true;
+        
+        else return false;
+        
     }
 };
